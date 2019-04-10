47cf8e7c020de2c5b44434c688228c4925db6e99
pixelvega <pixelvega.info@gmail.com>
2019-04-10 20:39:04 +0200

    Solve bug when handle after delete more than one article into a group

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,6 +7,7 @@ const cartArticlesEl = document.querySelector(".cart__articles");
 const cartSubtotal = document.querySelector(".cart__subtotal-sum");
 const cartTotal = document.querySelector(".cart__total-sum-number");
 const cartTotalArticles = document.querySelector(".cart__title-num");
+const itemQuantity = document.querySelector(".cart__item-quantity-number");
 
 const btnCartEl = document.querySelector(".header__icon-bag");
 const cartSection = document.querySelector(".page__cart");
@@ -23,20 +24,20 @@ const getResults = () => {
 };
 
 const getTotalAmount = data => {
+  let subtotal = 0;
   let total = 0;
-  let discount = 0;
 
   for (const item of data) {
-    total += item.price * item.units;
+    subtotal += item.price * item.units;
   }
-  if (total === 0) {
-    discount = 0;
+  if (subtotal === 0) {
+    total = 0;
   } else {
-    discount = 10;
+    total = subtotal - (subtotal * 30) / 100;
   }
 
-  cartSubtotal.innerHTML = total.toFixed(2);
-  cartTotal.innerHTML = (total - discount).toFixed(2);
+  cartSubtotal.innerHTML = subtotal.toFixed(2);
+  cartTotal.innerHTML = total.toFixed(2);
 };
 const getTotalArticles = data => {
   let totalArticles = 0;
@@ -49,6 +50,7 @@ const getTotalArticles = data => {
 
 const groupDataByDates = data => {
   let deliveryDates = [];
+  productsByDates = [];
   for (let i = 0; i < data.length; i++) {
     let product = data[i];
     let indexArr = deliveryDates.indexOf(product.date);
@@ -72,73 +74,77 @@ const showResults = productsByDates => {
   let header = "";
   let template = "";
   let date = "";
-  for (let i = 0; i < productsByDates.length; i++) {
-    date = productsByDates[i][i].date;
-    header = `
-    <ul class="cart__list">
-      <h5 class="cart__articles-delivery">Entrega ${date}</h5>
-    `;
-    for (let j = 0; j < productsByDates[i].length; j++) {
-      template = `
-      <li id="${productsByDates[i][j].id}" class="cart__item">
-        <div class="cart__item-wrapper">
-          <div class="cart__item-img-box">
-            <img
-              class="cart__item-img"
-              src="${productsByDates[i][j].image}"
-              alt="${productsByDates[i][j].name}"
-            />
-          </div>
-          <p class="cart__item-title">
-            <a class="cart__item-title">${productsByDates[i][j].name}</a>
-          </p>
-          <p class="cart__item-size">Talla: <span class="cart__item-size-number">${
-            productsByDates[i][j].size
-          }</span></p>
-          <p class="cart__item-price">
-            <span class="cart__item-price-text hidden">Price:</span>
-            <span class="cart__item-price-number">${
-              productsByDates[i][j].price
-            }</span>€
-          </p>
-          <div class="cart__item-quantity-controls">
-            <button class="btn cart__quantity-btn btn-plus" data-id="${
-              productsByDates[i][j].id
-            }" data-price="${productsByDates[i][j].price}" data-units="${
-        productsByDates[i][j].units
-      }">
-              <i class="fas fa-plus cart__quantity-icon"></i>
-            </button>
-            <div class="cart__item-quantity">
-              <span class="cart__item-quantity-number">${
-                productsByDates[i][j].units
-              }</span>
-            </div>
-            <button class="btn cart__quantity-btn btn-minus" data-id="${
-              productsByDates[i][j].id
-            }" data-price="${productsByDates[i][j].price}" data-units="${
-        productsByDates[i][j].units
-      }">
-              <i class="fas fa-minus cart__quantity-icon"></i>
-            </button>
-          </div>
-          <button class="btn cart__item-btn-remove" data-id="${
-            productsByDates[i][j].id
-          }">
-            <i class="far fa-trash-alt"></i>
-            <span class="cart__item-btn-remove-text">Eliminar</span>
-          </button>
-        </div>
-      </li>`;
-
-      listItems += template;
-      if (j === productsByDates[i].length - 1) {
-        cartList += header + listItems + "</ul>";
-        listItems = "";
-      }
-    }
-
+  if (productsByDates.length <= 0) {
     cartArticlesEl.innerHTML = cartList;
+  } else {
+    for (let i = 0; i < productsByDates.length; i++) {
+      date = productsByDates[i][i].date;
+      header = `
+      <ul class="cart__list">
+        <h5 class="cart__articles-delivery">Entrega ${date}</h5>
+      `;
+      for (let j = 0; j < productsByDates[i].length; j++) {
+        template = `
+        <li id="${productsByDates[i][j].id}" class="cart__item">
+          <div class="cart__item-wrapper">
+            <div class="cart__item-img-box">
+              <img
+                class="cart__item-img"
+                src="${productsByDates[i][j].image}"
+                alt="${productsByDates[i][j].name}"
+              />
+            </div>
+            <p class="cart__item-title">
+              <a class="cart__item-title">${productsByDates[i][j].name}</a>
+            </p>
+            <p class="cart__item-size">Talla: <span class="cart__item-size-number">${
+              productsByDates[i][j].size
+            }</span></p>
+            <p class="cart__item-price">
+              <span class="cart__item-price-text hidden">Price:</span>
+              <span class="cart__item-price-number">${
+                productsByDates[i][j].price
+              }</span>€
+            </p>
+            <div class="cart__item-quantity-controls">
+              <button class="btn cart__quantity-btn btn-plus" data-id="${
+                productsByDates[i][j].id
+              }">
+                <i class="fas fa-plus cart__quantity-icon"></i>
+              </button>
+              <div class="cart__item-quantity">
+                <span class="cart__item-quantity-number cart__item-quantity-number${
+                  productsByDates[i][j].id
+                }" data-id="${productsByDates[i][j].id}">${
+          productsByDates[i][j].units
+        }</span>
+              </div>
+              <button class="btn cart__quantity-btn btn-minus" data-id="${
+                productsByDates[i][j].id
+              }" data-price="${productsByDates[i][j].price}" data-units="${
+          productsByDates[i][j].units
+        }">
+                <i class="fas fa-minus cart__quantity-icon"></i>
+              </button>
+            </div>
+            <button class="btn cart__item-btn-remove" data-id="${
+              productsByDates[i][j].id
+            }">
+              <i class="far fa-trash-alt"></i>
+              <span class="cart__item-btn-remove-text">Eliminar</span>
+            </button>
+          </div>
+        </li>`;
+
+        listItems += template;
+        if (j === productsByDates[i].length - 1) {
+          cartList += header + listItems + "</ul>";
+          listItems = "";
+        }
+      }
+
+      cartArticlesEl.innerHTML = cartList;
+    }
   }
 
   addListeners();
@@ -162,33 +168,44 @@ const addListeners = () => {
 
 const handleMinus = e => {
   const itemId = e.currentTarget.getAttribute("data-id");
+  const itemQuantity = document.querySelector(`span[data-id="${itemId}"]`);
 
-  if (data[itemId].units - 1 > 0) {
-    --data[itemId].units;
-
-    groupDataByDates(data);
+  for (const item of data) {
+    if (item.id === itemId && item.units - 1 > 0) {
+      --item.units;
+      itemQuantity.innerHTML = item.units;
+    }
   }
+  getTotalAmount(data);
+  getTotalArticles(data);
 };
 
 const handlePlus = e => {
   const itemId = e.currentTarget.getAttribute("data-id");
-
-  ++data[itemId].units;
-
-  groupDataByDates(data);
+  const itemQuantity = document.querySelector(`span[data-id="${itemId}"]`);
+  for (const item of data) {
+    if (item.id === itemId) {
+      ++item.units;
+      itemQuantity.innerHTML = item.units;
+    }
+  }
+  getTotalAmount(data);
+  getTotalArticles(data);
 };
 
 const handleRemoveItem = e => {
   const itemId = e.currentTarget.getAttribute("data-id");
+
   const itemEl = document.getElementById(`${itemId}`);
   const parentEl = itemEl.parentNode;
-
-  parentEl.removeChild(itemEl);
-
+  for (const item of data) {
+    if (item.id === itemId) {
+      parentEl.removeChild(itemEl);
+    }
+  }
   if (parentEl.children.length <= 1) {
     parentEl.parentNode.removeChild(parentEl);
   }
-
   for (let i = 0; i < data.length; i++) {
     if (data[i].id.indexOf(itemId) !== -1) {
       data.splice(i, 1);
